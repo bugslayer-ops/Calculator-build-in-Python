ee77aa03a15193ad50cafef1601679da5535d778
Discord Programmers <78758318+bugslayer-ops@users.noreply.github.com>
2022-06-12 15:57:32 +0530

    updated calculator.py

--- calculator.py
+++ calculator.py
@@ -16,7 +16,28 @@ def calculator():
     if Input == 1: # usr input "1"
         os.system("cls") # clear screen 
         print("Note:- You Selected Basic Calculations..!") # usr selected Basic Calculations
-        
+        menu.Menu().basic_cal()
+        Input = int(input("Enter Your Choice: "))
+        if Input == 1: # code written in this is for addition 
+            result = functions.basic_method().add() # usr define function module
+            print("Result of Addition is:",result)
+            time.sleep(7)
+            os.system("cls")
+        if Input == 2: # code written in this is for subtraction 
+            result = functions.basic_method().subtract()
+            print("Result of Subtraction is:",result)
+            time.sleep(7)
+            os.system("cls")
+        if Input == 3: # code written in this is for multiplication 
+            result = functions.basic_method().multiply()
+            print("Result of Multiplication is:",result)
+            time.sleep(7)
+            os.system("cls")
+        if Input == 4: # code written in this is for division 
+            result = functions.basic_method().divide()
+            print("Result of Division is:",result)
+            time.sleep(7)
+            os.system("cls")
 calculator()
     
 
