9da6ce0bab7bab4d7e6d77f65ca6df8fd1204812
Discord Programmers <78758318+bugslayer-ops@users.noreply.github.com>
2022-06-12 15:56:29 +0530

    updated functions.py

--- functions.py
+++ functions.py
@@ -2,9 +2,42 @@ import functools
 
 class basic_method:
     def add(self):
+        # x = int(input("Enter Value: "))
+        # y = int(input("Enter Value: "))
         num = []
         x = ""
-        while x == 'q':
+        while x !='q':
             x = input("Enter Value: ")
-            if x=='q':
+            if x == 'q':
                 break
+            num.append(x)
+        num = list(map(int, num))
+        result  = functools.reduce(lambda x,y: x+y,num)
+        return result
+    def subtract(self):
+        num = []
+        x = ""
+        while x != 'q':
+            x = input("Enter Value: ")
+            if x == 'q':
+                break
+            num.append(x)
+        num = list(map(int,num))
+        result = functools.reduce(lambda x,y: x-y,num)
+        return result
+    def multiply(self):
+        num = []
+        x = ""
+        while x != 'q':
+            x  = input("Enter Value: ")
+            if x == 'q':
+                break
+            num.append(x)
+        num = list(map(int,num))
+        result = functools.reduce(lambda x,y: x*y,num)
+    def divide(self):
+        x = int(input("Enter Value: "))
+        y = int(input("Enter Value: "))
+        return x/y
+
+    
